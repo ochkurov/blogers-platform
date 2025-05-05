e067812cd0b1771835b50b526bc312433dd22eb0
chik1313 <ochkurov96@gmail.com>
2025-05-05 16:52:23 +0200

    add user model schema

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -1,3 +1,10 @@
 import { INestApplication } from '@nestjs/common';
+import { pipesSetup } from './pipes.setup';
+import { globalPrefixSetup } from './global-prefix.setup';
 
-export function appSetup(app: INestApplication) {}
+export function appSetup(app: INestApplication) {
+  pipesSetup(app);
+  globalPrefixSetup(app);
+  swaggerSetup(app);
+  //...
+}
